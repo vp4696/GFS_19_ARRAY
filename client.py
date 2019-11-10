fd446bee566a9d6d0df4c1c1ee8ecc8239ff5890
Vivek Puar <53707900+vp4696@users.noreply.github.com>
2019-11-10 23:13:20 +0530

    Add files via upload

--- client.py
+++ client.py
@@ -2,6 +2,7 @@ import socket
 import os
 import pickle
 
+
 def connect_to_master_server():
     s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     s.connect((socket.gethostbyname('localhost'),7082))
@@ -15,11 +16,34 @@ def connect_to_master_server():
     return chunks
 
 def connect_to_chunk_server(chunks):
-    list1=[5067,5068]
-    for i in list1:
+    list1=[6467,6468,6469,6470]
+    '''
+        for i in list1:
         s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         s.connect((socket.gethostbyname('localhost'),i))
         s.send(b"Hi from Client")
+        '''
+    chunks_list=[]
+    f=open("a.txt",'rb')
+    data=f.read(200)
+    #size=os.path.getsize("six.mp3")
+    print(len(chunks))
+
+
+    while data:
+        chunks_list.append(data)
+        data=f.read(200)
+    
+    for chunk_id,chunk_server in chunks:
+        s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        s.connect((socket.gethostbyname('localhost'),list1[chunk_server-1]))
+        to_send=str(chunk_server)+":"+str(chunk_id)
+        s.send(str(to_send).encode("utf-8"))
+        s.send(chunks_list[chunk_id-1])
+        
+
+
+        
     
 
 
