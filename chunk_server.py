52515647424b2dde679f394826515ea576c612e6
swapnil-satpathy <54858475+swapnil-satpathy@users.noreply.github.com>
2019-11-12 02:22:40 +0530

    Add files via upload

--- chunk_server.py
+++ chunk_server.py
@@ -4,9 +4,9 @@ import os
 import math
 import pickle
 
-MAX=2048
-
 class ChunkServer(object):
+
+    
     
     def __init__(self, host, port):
         self.filesystem=""
@@ -16,6 +16,8 @@ class ChunkServer(object):
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock.bind((self.host, self.port))
 
+
+    #listening to connections and after accepting makes a new thread for every connection
     def listen(self):
         self.sock.listen(5)
         while True:
@@ -23,16 +25,46 @@ class ChunkServer(object):
             client.settimeout(60)
             threading.Thread(target = self.listenToClient,args = (client,address)).start()
 
+
+
+
+
+
+    def connect_to_master(self):
+        s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        s.connect((socket.gethostbyname('localhost'),7082))
+        s.send(b"chunkserver:gobble:google")
+
+
+
+
+
+
+
+    #The thread at work...accpeting each chunk from the client and storing in its directory
+
     def listenToClient(self, client, address):
-        to_recv=client.recv(400).decode("utf-8")                    #Padding done with '~' at the end
-        chunk_server_no,chunk_id,dummy=to_recv.split(":")
+
+        to_recv=client.recv(400).decode("utf-8")
+        # chunk_server=client.recv(2048).decode("utf-8")
+        # print(chunk_id)
+        # print(chunk_server)
+        
+        # # print(to_recv)
+
+        chunk_server_no,chunk_id,filenaming,dummy=to_recv.split(":")
         self.filesystem = os.getcwd()+"/"+str(chunk_server_no)
+        print(self.filesystem)
         if not os.access(self.filesystem, os.W_OK):
             os.makedirs(self.filesystem)
-        filename = self.filesystem+"/"+chunk_id
-        with open(filename, "wb") as f:
-            chunks_recv=client.recv(MAX)
-            f.write(chunks_recv)
+        
+        filename = self.filesystem+"/"+str(filenaming)+str(chunk_id)
+        print(filename)
+        with open(filename, "w") as f:
+            chunks_recv=client.recv(2048)
+            f.write(chunks_recv.decode("utf-8"))
+        self.connect_to_master()
+        
 
         
 
@@ -46,3 +78,5 @@ if __name__ == "__main__":
             pass
     print("Chunk Server Running")
     ChunkServer('',port_num).listen()
+
+    
